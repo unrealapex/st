7ff651189a634d465844ac77d78211f8692677d7
Aaron John <unrealapex@proton.me>
2024-02-28 20:16:15 -0800

    tweak: increase opacity to 80%

--- config.h
+++ config.h
@@ -95,7 +95,7 @@ char *termname = "xterm-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.65;
+float alpha = 0.80;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
