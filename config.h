8f836fbd5d0b0cb83be3555873bacf153ddd2b5b
Aaron John <unrealapex@proton.me>
2024-03-04 13:07:26 -0800

    fix: jerky scrolling

--- config.h
+++ config.h
@@ -221,8 +221,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Up,          kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Down,        kscrolldown,    {.i = -1} },
+	{ ShiftMask,            XK_Up,          kscrollup,      {.i =  1} },
+	{ ShiftMask,            XK_Down,        kscrolldown,    {.i =  1} },
 	{ TERMMOD,              XK_F,           externalpipe,   {.v = pagersearchcmd} },
 };
 
