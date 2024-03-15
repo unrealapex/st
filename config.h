1c8df23594451eb837c16688bd1f57c50371de6e
Aaron John <unrealapex@proton.me>
2024-03-15 12:03:05 -0700

    tweak: use noto color emoji as fallback font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Jetbrains Mono Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
-static char *font2 = "Segoe UI Emoji:pixelsize=16:antialias=true:autohint=true";
+static char *font2 = "Noto Color Emoji:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
